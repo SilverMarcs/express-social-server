27a9d28b99fd41b0697bc0151e4fa0d0d2733430
Zabir Raihan <zabirraihan6@gmail.com>
2023-06-23 09:27:36 +0600

    Not working posts widgets and friend component

--- routes/posts.js
+++ routes/posts.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 /* read */
 router.get("/", verifyToken, getFeedPosts); // for now, getFeedPosts will return all posts. later, we will implement puling posts only from friends. TODO
-router.get("/:userId/posts", verifyToken, getUserPosts); // querying posts only for a particular user. in my profile, I should only see my posts
+router.get("/:userId/", verifyToken, getUserPosts); // querying posts only for a particular user. in my profile, I should only see my posts
 
 /* update */
 router.patch("/:id/like", verifyToken, likePost); //passing in the id of the post to like
