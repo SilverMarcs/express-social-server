dee9d8cf542022015570e71fe27fd1232a10ca97
Zabir Raihan <zabirraihan6@gmail.com>
2023-06-27 01:54:44 +0600

    Early impl user search navbar

--- controllers/users.js
+++ controllers/users.js
@@ -68,3 +68,24 @@ export const addRemoveFriend = async (req, res) => {
     res.status(404).json({ error: err.message });
   }
 };
+
+export const searchUsersByFirstName = async (req, res) => {
+  try {
+    const { firstName } = req.params;
+    const users = await User.find({
+      firstName: { $regex: firstName.toString(), $options: "i" },
+    });
+
+    const formattedUsers = users.map(
+      ({ _id, firstName, lastName, occupation, picturePath }) => {
+        return { _id, firstName, lastName, occupation, picturePath };
+      }
+    );
+
+    console.log(`formattedUsers`, formattedUsers);
+
+    res.status(200).json(formattedUsers);
+  } catch (err) {
+    res.status(404).json({ error: err.message });
+  }
+};
--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ import {
   addRemoveFriend,
   getUser,
   getUserFriends,
+  searchUsersByFirstName,
 } from "../controllers/users.js";
 import { verifyToken } from "../middleware/auth.js";
 
@@ -11,6 +12,7 @@ const router = express.Router();
 /* read */
 router.get("/:id", verifyToken, getUser); // this line means that when a GET request is sent to the '/users/:id' route, the getUser function will be called. basically when the frontend sends an id, we can query the database with this id
 router.get("/:id/friends", verifyToken, getUserFriends);
+router.get("/search/:firstName", verifyToken, searchUsersByFirstName);
 
 /* update */
 router.patch("/:id/:friendId", verifyToken, addRemoveFriend); //passing in the id of the user and the id of the friend to add/remove
